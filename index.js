03165b9fa6d75a6a119c02629825811f1c632828
Ariana <ariana.morin214@gmail.com>
2024-01-06 12:20:10 -0600

    Added JS for button click and typewriter effect for poem.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Whose woods these are I think I know.",
+    autoStart: true,
+    delay: 40,
+  });
+}
 
+let poemFormElement = document.querySelector("#searching");
+poemFormElement.addEventListener("submit", generatePoem);
